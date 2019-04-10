0c588a063b20d6af5d2968e6c4603dd35e3577ce
David Joy <djoy@edx.org>
2019-04-10 12:28:58 -0400

    feat(actions): export more actions for userAccount
    It’s useful in consuming apps to be able to hook into the fetch user account lifecycle.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import { fetchUserAccount, fetchUserAccountSuccess } from './actions/userAccount';
+import {
+  fetchUserAccount,
+  fetchUserAccountBegin,
+  fetchUserAccountFailure,
+  fetchUserAccountSuccess,
+  FETCH_USER_ACCOUNT_BEGIN,
+  FETCH_USER_ACCOUNT_FAILURE,
+  FETCH_USER_ACCOUNT_SUCCESS,
+} from './actions/userAccount';
 import getAuthenticatedAPIClient from './AuthenticatedAPIClient';
 import PrivateRoute from './PrivateRoute';
 import userAccount from './reducers/userAccount';
@@ -6,7 +14,12 @@ import UserAccountApiService from './services/UserAccountApiService';
 
 export {
   fetchUserAccount,
+  fetchUserAccountBegin,
+  fetchUserAccountFailure,
   fetchUserAccountSuccess,
+  FETCH_USER_ACCOUNT_BEGIN,
+  FETCH_USER_ACCOUNT_FAILURE,
+  FETCH_USER_ACCOUNT_SUCCESS,
   getAuthenticatedAPIClient,
   PrivateRoute,
   userAccount,
