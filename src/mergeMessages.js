166c853ab5350db7e81ded7aba3fc3ec0afaee56
Adam Butterworth <abutterworth@edx.org>
2019-09-18 10:01:45 -0400

    fix: merge messages deeply

--- src/mergeMessages.js
+++ src/mergeMessages.js
@@ -1,3 +1,5 @@
+import merge from 'lodash.merge';
+
 export default function mergeMessages(messagesArray = []) {
-  return Array.isArray(messagesArray) ? Object.assign({}, ...messagesArray) : {};
+  return Array.isArray(messagesArray) ? merge({}, ...messagesArray) : {};
 }
--- src/mergeMessages.test.js
+++ src/mergeMessages.test.js
@@ -10,6 +10,31 @@ describe('mergeMessages', () => {
     });
   });
 
+  it('should merge nested objects from an array', () => {
+    const messages = [
+      {
+        en: { hello: 'hello' },
+        es: { hello: 'hola' },
+      },
+      {
+        en: { goodbye: 'goodbye' },
+        es: { goodbye: 'adiós' },
+      },
+    ];
+
+    const result = mergeMessages(messages);
+    expect(result).toEqual({
+      en: {
+        hello: 'hello',
+        goodbye: 'goodbye',
+      },
+      es: {
+        hello: 'hola',
+        goodbye: 'adiós',
+      },
+    });
+  });
+
   it('should return an empty object if no messages', () => {
     expect(mergeMessages(undefined)).toEqual({});
     expect(mergeMessages(null)).toEqual({});
