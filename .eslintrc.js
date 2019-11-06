15c2b97b6e1bf902eaebaa561b90792ef51ece4d
Adam Butterworth <abutterworth@edx.org>
2019-11-06 17:18:12 -0500

    add linter and install many dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,24 @@
 const { createConfig } = require('@edx/frontend-build');
 
-module.exports = createConfig('eslint');
+module.exports = createConfig('eslint', {
+  rules: {
+    "no-trailing-spaces":  [
+      "error",
+      {
+        "ignoreComments": true,
+      },
+    ],
+    "max-len": [
+      "error",
+      120,
+      2,
+      {
+        "ignoreUrls": true,
+        "ignoreComments": false,
+        "ignoreRegExpLiterals": true,
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true,
+      },
+    ],
+  },
+});
