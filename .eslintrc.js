d3c23d911fe099c12a53e7dfab421e9c8fc797ae
Adam Butterworth <abutterworth@edx.org>
2019-11-08 12:51:10 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,26 @@ const { getBaseConfig } = require('@edx/frontend-build');
 const config = getBaseConfig('eslint');
 
 config.rules = {
-  "import/no-extraneous-dependencies": [
-    "error",
-    {
-      "devDependencies": [
-        "**/*.config.js",
-        "**/*.test.jsx",
-        "**/*.test.js",
-        "example/*",
-      ],
-    },
-  ],
+  'import/no-extraneous-dependencies': ['error', {
+    'devDependencies': [
+      '**/*.config.js',
+      '**/*.test.jsx',
+      '**/*.test.js',
+      'example/*',
+    ],
+  }],
+  'import/extensions': ['error', {
+    ignore: ['@edx/frontend-platform*'],
+  }],
+  'import/no-unresolved': ['error', {
+    ignore: ['@edx/frontend-platform*'],
+  }],
+  'jsx-a11y/anchor-is-valid': ['error', {
+    components: ['Link'],
+    specialLink: [],
+    specialLink: ['to'],
+    aspects: ['noHref', 'invalidHref', 'preferButton'],
+  }],
 };
 
 module.exports = config;
