8468d5a5e2ef4a0836ce15ad864a744a8c4e9d44
Adam Butterworth <abutterworth@edx.org>
2019-11-18 12:45:33 -0500

    errors fixed warnings remain

--- example/index.jsx
+++ example/index.jsx
@@ -3,27 +3,29 @@ import 'babel-polyfill'; // eslint-disable-line import/no-extraneous-dependencie
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route } from 'react-router-dom';
-import { App, AppProvider, APP_ERROR, APP_READY, ErrorPage, AuthenticatedRoute } from '@edx/frontend-platform/base';
+import { AppProvider, ErrorPage, AuthenticatedPageRoute } from '@edx/frontend-platform/react';
+import { APP_ERROR, APP_READY, initialize } from '@edx/frontend-platform/init';
+import { subscribe } from '@edx/frontend-platform/pubSub';
 
 import './index.scss';
 import ExamplePage from './ExamplePage';
 import AuthenticatedPage from './AuthenticatedPage';
 
-App.subscribe(APP_READY, () => {
+subscribe(APP_READY, () => {
   ReactDOM.render(
     <AppProvider>
       <Route exact path="/" component={ExamplePage} />
-      <AuthenticatedRoute exact path="/authenticated" component={AuthenticatedPage} />
+      <AuthenticatedPageRoute exact path="/authenticated" component={AuthenticatedPage} />
     </AppProvider>,
     document.getElementById('root'),
   );
 });
 
-App.subscribe(APP_ERROR, (error) => {
+subscribe(APP_ERROR, (error) => {
   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
 });
 
-App.initialize({
+initialize({
   messages: [],
   requireAuthenticatedUser: false,
   hydrateAuthenticatedUser: true,
