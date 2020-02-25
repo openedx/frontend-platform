27d52fd53a377d46e4e8a1f1383f2c40a444aeb0
David Joy <djoy@edx.org>
2020-02-25 15:13:26 -0500

    feat: More flexible page routes
    - Added redirectUrl prop to AuthenticatedPageRoute to enable redirection to pages other than login.
    - Refactored PageRoute to allow all react-router-dom Route props
    - Added getLoginRedirectUrl and getLogoutRedirectUrl for direct access to login/logout URL creation.

--- src/react/AuthenticatedPageRoute.jsx
+++ src/react/AuthenticatedPageRoute.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
-import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { useRouteMatch } from 'react-router-dom';
 
 import AppContext from './AppContext';
-import LoginRedirect from './LoginRedirect';
 import PageRoute from './PageRoute';
+import { getLoginRedirectUrl } from '../auth';
 
 /**
  * A react-router route that redirects to the login page when the route becomes active and the user
@@ -11,22 +12,49 @@ import PageRoute from './PageRoute';
  * false, an authenticatedPageRoute can be used to protect a subset of the application's routes,
  * rather than the entire application.
  *
+ * It can optionally accept an override URL to redirect to instead of the login page.
+ *
  * Like a `PageRoute`, also calls `sendPageEvent` when the route becomes active.
  *
  * @see PageRoute
  * @see {@link module:frontend-platform/analytics~sendPageEvent}
  * @memberof module:React
  * @param {Object} props
+ * @param {string} props.redirectUrl The URL anonymous users should be redirected to, rather than
+ * viewing the route's contents.
  */
-export default function AuthenticatedPageRoute(props) {
+export default function AuthenticatedPageRoute({ redirectUrl, ...props }) {
   const { authenticatedUser } = useContext(AppContext);
 
-  // We need to let Route "render" the redirect - if we did it here in AuthenticatedRoute, it'd
-  // execute immediately, rather than when the router determines the route should become active.
+  const match = useRouteMatch({
+    // eslint-disable-next-line react/prop-types
+    path: props.path,
+    // eslint-disable-next-line react/prop-types
+    exact: props.exact,
+    // eslint-disable-next-line react/prop-types
+    strict: props.strict,
+    // eslint-disable-next-line react/prop-types
+    sensitive: props.sensitive,
+  });
+
   if (authenticatedUser === null) {
-    return <Route {...props} component={LoginRedirect} />;
+    if (match) {
+      const destination = redirectUrl || getLoginRedirectUrl(global.location.href);
+      global.location.assign(destination);
+    }
+    // This emulates a Route's way of displaying nothing if the route's path doesn't match the
+    // current URL.
+    return null;
   }
   return (
     <PageRoute {...props} />
   );
 }
+
+AuthenticatedPageRoute.propTypes = {
+  redirectUrl: PropTypes.string,
+};
+
+AuthenticatedPageRoute.defaultProps = {
+  redirectUrl: null,
+};
--- src/react/AuthenticatedPageRoute.test.jsx
+++ src/react/AuthenticatedPageRoute.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { Router, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import { getAuthenticatedUser, redirectToLogin } from '../auth';
+import { getAuthenticatedUser, getLoginRedirectUrl } from '../auth';
 import AuthenticatedPageRoute from './AuthenticatedPageRoute';
 import AppContext from './AppContext';
 import { getConfig } from '../config';
@@ -12,17 +12,25 @@ jest.mock('../analytics');
 jest.mock('../auth');
 
 describe('AuthenticatedPageRoute', () => {
+  const { location } = global;
   let history;
 
   beforeEach(() => {
+    delete global.location.assign;
+    global.location.assign = jest.fn();
     sendPageEvent.mockReset();
-    redirectToLogin.mockReset();
+    getLoginRedirectUrl.mockReset();
     getAuthenticatedUser.mockReset();
     history = createBrowserHistory();
   });
 
-  it('should call login if not authenticated', () => {
+  afterEach(() => {
+    global.location = location;
+  });
+
+  it('should redirect to login if not authenticated', () => {
     getAuthenticatedUser.mockReturnValue(null);
+    getLoginRedirectUrl.mockReturnValue('http://localhost/login?next=http%3A%2F%2Flocalhost%2Fauthenticated');
     const component = (
       <AppContext.Provider
         value={{
@@ -38,13 +46,37 @@ describe('AuthenticatedPageRoute', () => {
     );
     history.push('/authenticated');
     mount(component);
+    expect(getLoginRedirectUrl).toHaveBeenCalledWith('http://localhost/authenticated');
     expect(sendPageEvent).not.toHaveBeenCalled();
-    expect(redirectToLogin).toHaveBeenCalledWith('http://localhost/authenticated');
+    expect(global.location.assign).toHaveBeenCalledWith('http://localhost/login?next=http%3A%2F%2Flocalhost%2Fauthenticated');
+  });
+
+  it('should redirect to custom redirect URL if not authenticated', () => {
+    getAuthenticatedUser.mockReturnValue(null);
+    getLoginRedirectUrl.mockReturnValue('http://localhost/login?next=http%3A%2F%2Flocalhost%2Fauthenticated');
+    const component = (
+      <AppContext.Provider
+        value={{
+          authenticatedUser: getAuthenticatedUser(),
+          config: getConfig(),
+        }}
+      >
+        <Router history={history}>
+          <Route exact path="/" component={() => <p>Anonymous</p>} />
+          <AuthenticatedPageRoute redirectUrl="http://localhost/elsewhere" path="/authenticated" component={() => <p>Authenticated</p>} />
+        </Router>
+      </AppContext.Provider>
+    );
+    history.push('/authenticated');
+    mount(component);
+    expect(getLoginRedirectUrl).not.toHaveBeenCalled();
+    expect(sendPageEvent).not.toHaveBeenCalled();
+    expect(global.location.assign).toHaveBeenCalledWith('http://localhost/elsewhere');
   });
 
   it('should not call login if not the current route', () => {
     getAuthenticatedUser.mockReturnValue(null);
-
+    getLoginRedirectUrl.mockReturnValue('http://localhost/login?next=http%3A%2F%2Flocalhost%2Fauthenticated');
     const component = (
       <AppContext.Provider
         value={{
@@ -61,7 +93,8 @@ describe('AuthenticatedPageRoute', () => {
     history.push('/');
     const wrapper = mount(component);
 
-    expect(redirectToLogin).not.toHaveBeenCalled();
+    expect(getLoginRedirectUrl).not.toHaveBeenCalled();
+    expect(global.location.assign).not.toHaveBeenCalled();
     expect(sendPageEvent).not.toHaveBeenCalled();
     const element = wrapper.find('p');
     expect(element.text()).toEqual('Anonymous'); // This is just a sanity check on our setup.
@@ -83,7 +116,8 @@ describe('AuthenticatedPageRoute', () => {
     );
     history.push('/authenticated');
     const wrapper = mount(component);
-    expect(redirectToLogin).not.toHaveBeenCalled();
+    expect(getLoginRedirectUrl).not.toHaveBeenCalled();
+    expect(global.location.assign).not.toHaveBeenCalled();
     expect(sendPageEvent).toHaveBeenCalled();
     const element = wrapper.find('p');
     expect(element.text()).toEqual('Authenticated');
