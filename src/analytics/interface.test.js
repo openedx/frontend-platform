1de216a528856da98d1a680a6aeb8115d1984471
David Joy <djoy@edx.org>
2021-05-04 12:04:12 -0400

    fix: only initialize Segment if a SEGMENT_KEY exists
    This gates segment initialization on the presence of a valid SEGMENT_KEY from configuration.  A truthy SEGMENT_KEY is assumed to be valid.
    
    If segment isn’t initialized, the SegmentAnalyticsService now short circuits any Segment analytics functions that might have been called.  Non-segment service methods will still work. (i.e., tracking log events)
    
    Not having a SEGMENT_KEY should be a valid configuration, especially for Open edX operators who don’t use segment.  Given that micro-frontend consumers of the analytics interface _assume_ that they can call it with impunity and without error, the service now errs on the side of silently doing nothing, rather than complaining if SEGMENT_KEY isn’t present and a Segment-related function is called.

--- src/analytics/interface.test.js
+++ src/analytics/interface.test.js
@@ -30,146 +30,217 @@ mockAuthApiClient.post = jest.fn().mockResolvedValue(undefined);
 // in the document. Add one here.
 document.body.innerHTML = '<script id="stub" />';
 
-beforeEach(() => {
-  configure(SegmentAnalyticsService, {
-    loggingService: mockLoggingService,
-    httpClient: mockAuthApiClient,
-    config: {
-      LMS_BASE_URL: 'https://example.com',
-      SEGMENT_KEY: 'test-key',
-    },
+describe('Analytics', () => {
+  let service;
+  let userIdFunction;
+
+  beforeEach(() => {
+    window.analytics = [];
   });
-  mockLoggingService.logError.mockReset();
-  mockAuthApiClient.post.mockReset();
-  mockAuthApiClient.post.mockResolvedValue(undefined);
-  window.analytics.identify = jest.fn();
-  window.analytics.page = jest.fn();
-  window.analytics.track = jest.fn();
-  window.analytics.reset = jest.fn();
-  window.analytics.ready = jest.fn((callback) => {
-    if (callback) {
-      callback();
+
+  function afterConfigure() {
+    mockLoggingService.logError.mockReset();
+    mockAuthApiClient.post.mockReset();
+    mockAuthApiClient.post.mockResolvedValue(undefined);
+    window.analytics.identify = jest.fn();
+    window.analytics.page = jest.fn();
+    window.analytics.track = jest.fn();
+    window.analytics.reset = jest.fn();
+    userIdFunction = jest.fn();
+    window.analytics.user = jest.fn(() => ({
+      id: userIdFunction,
+    }));
+    window.analytics.ready = jest.fn((callback) => {
+      if (callback) {
+        callback();
+      }
+    });
+  }
+
+  describe('with valid SEGMENT_KEY', () => {
+    beforeEach(() => {
+      service = configure(SegmentAnalyticsService, {
+        loggingService: mockLoggingService,
+        httpClient: mockAuthApiClient,
+        config: {
+          LMS_BASE_URL: 'https://example.com',
+          SEGMENT_KEY: 'test-key',
+        },
+      });
+
+      afterConfigure();
+
+      expect(global.analytics.invoked).toBe(true);
+      expect(service.segmentInitialized).toBe(true);
+    });
+
+    describe('analytics sendTrackingLogEvent', () => {
+      it('posts expected data when successful', () => {
+        expect.assertions(6);
+        return sendTrackingLogEvent(eventType, eventData)
+          .then(() => {
+            expect(mockAuthApiClient.post.mock.calls.length).toEqual(1);
+            expect(mockAuthApiClient.post.mock.calls[0][0]).toEqual('https://example.com/event');
+            const expectedData = 'event_type=test.event&event=%7B%22test_shallow%22%3A%22test-shallow%22%2C%22test_object%22%3A%7B%22test_deep%22%3A%22test-deep%22%7D%7D&page=http%3A%2F%2Flocalhost%2F';
+            expect(mockAuthApiClient.post.mock.calls[0][1]).toEqual(expectedData);
+            const config = mockAuthApiClient.post.mock.calls[0][2];
+            expect(config.headers['Content-Type']).toEqual('application/x-www-form-urlencoded');
+          });
+      });
+
+      it('calls loggingService.logError on error', () => {
+        mockAuthApiClient.post.mockRejectedValue('test-error');
+        expect.assertions(4);
+        return sendTrackingLogEvent(eventType, eventData)
+          .then(() => {
+            expect(mockLoggingService.logError.mock.calls.length).toBe(1);
+            expect(mockLoggingService.logError).toBeCalledWith('test-error');
+          });
+      });
+    });
+
+    describe('analytics identifyAuthenticatedUser', () => {
+      it('calls Segment identify on success', () => {
+        const testTraits = { anything: 'Yay!' };
+        identifyAuthenticatedUser(testUserId, testTraits);
+
+        expect(window.analytics.identify.mock.calls.length).toBe(1);
+        expect(window.analytics.identify).toBeCalledWith(testUserId, testTraits);
+      });
+
+      it('throws error if userId is not supplied', () => {
+        expect(() => identifyAuthenticatedUser(null))
+          .toThrowError(new Error('UserId is required for identifyAuthenticatedUser.'));
+      });
+    });
+
+    describe('analytics identifyAnonymousUser', () => {
+      it('calls Segment identify on success - no previous segment user', () => {
+        window.analytics.user = () => ({ id: () => null });
+        const testTraits = { anything: 'Yay!' };
+        identifyAnonymousUser(testTraits);
+
+        expect(window.analytics.reset.mock.calls.length).toBe(0);
+        expect(window.analytics.identify.mock.calls.length).toBe(1);
+        expect(window.analytics.identify).toBeCalledWith(testTraits);
+      });
+
+      it('calls Segment identify on success - previous segment user', () => {
+        window.analytics.user = () => ({ id: () => 7 });
+        const testTraits = { anything: 'Yay!' };
+        identifyAnonymousUser(testTraits);
+
+        expect(window.analytics.reset.mock.calls.length).toBe(1);
+        expect(window.analytics.identify.mock.calls.length).toBe(1);
+        expect(window.analytics.identify).toBeCalledWith(testTraits);
+      });
+    });
+
+    function testSendPageAfterIdentify(identifyFunction) {
+      identifyFunction();
+
+      const testCategory = 'test-category';
+      const testName = 'test-name';
+      const testProperties = { anything: 'Yay!' };
+      sendPageEvent(testCategory, testName, testProperties);
+
+      expect(window.analytics.page.mock.calls.length).toBe(1);
+      expect(window.analytics.page).toBeCalledWith(testCategory, testName, testProperties);
     }
-  });
-});
 
-describe('analytics sendTrackingLogEvent', () => {
-  it('posts expected data when successful', () => {
-    expect.assertions(4);
-    return sendTrackingLogEvent(eventType, eventData)
-      .then(() => {
-        expect(mockAuthApiClient.post.mock.calls.length).toEqual(1);
-        expect(mockAuthApiClient.post.mock.calls[0][0]).toEqual('https://example.com/event');
-        const expectedData = 'event_type=test.event&event=%7B%22test_shallow%22%3A%22test-shallow%22%2C%22test_object%22%3A%7B%22test_deep%22%3A%22test-deep%22%7D%7D&page=http%3A%2F%2Flocalhost%2F';
-        expect(mockAuthApiClient.post.mock.calls[0][1]).toEqual(expectedData);
-        const config = mockAuthApiClient.post.mock.calls[0][2];
-        expect(config.headers['Content-Type']).toEqual('application/x-www-form-urlencoded');
+    describe('analytics send Page event', () => {
+      it('calls Segment page on success after identifyAuthenticatedUser', () => {
+        const userId = 1;
+        testSendPageAfterIdentify(() => identifyAuthenticatedUser(userId));
       });
-  });
 
-  it('calls loggingService.logError on error', () => {
-    mockAuthApiClient.post.mockRejectedValue('test-error');
-    expect.assertions(2);
-    return sendTrackingLogEvent(eventType, eventData)
-      .then(() => {
+      it('calls Segment page on success after identifyAnonymousUser', () => {
+        testSendPageAfterIdentify(identifyAnonymousUser);
+      });
+
+      it('fails if page called with no identify', () => {
+        sendPageEvent();
+
         expect(mockLoggingService.logError.mock.calls.length).toBe(1);
-        expect(mockLoggingService.logError).toBeCalledWith('test-error');
+        expect(mockLoggingService.logError).toBeCalledWith('Identify must be called before other tracking events.');
       });
-  });
-});
+    });
 
-describe('analytics identifyAuthenticatedUser', () => {
-  it('calls Segment identify on success', () => {
-    const testTraits = { anything: 'Yay!' };
-    identifyAuthenticatedUser(testUserId, testTraits);
+    function testSendTrackEventAfterIdentify(identifyFunction) {
+      identifyFunction();
 
-    expect(window.analytics.identify.mock.calls.length).toBe(1);
-    expect(window.analytics.identify).toBeCalledWith(testUserId, testTraits);
-  });
+      const testName = 'test-name';
+      const testProperties = { anything: 'Yay!' };
+      sendTrackEvent(testName, testProperties);
 
-  it('throws error if userId is not supplied', () => {
-    expect(() => identifyAuthenticatedUser(null))
-      .toThrowError(new Error('UserId is required for identifyAuthenticatedUser.'));
-  });
-});
+      expect(window.analytics.track.mock.calls.length).toBe(1);
+      expect(window.analytics.track).toBeCalledWith(testName, testProperties);
+    }
 
-describe('analytics identifyAnonymousUser', () => {
-  it('calls Segment identify on success - no previous segment user', () => {
-    window.analytics.user = () => ({ id: () => null });
-    const testTraits = { anything: 'Yay!' };
-    identifyAnonymousUser(testTraits);
+    describe('analytics send Track event', () => {
+      it('calls Segment track on success after identifyAuthenticatedUser', () => {
+        const userId = 1;
+        testSendTrackEventAfterIdentify(() => identifyAuthenticatedUser(userId));
+      });
 
-    expect(window.analytics.reset.mock.calls.length).toBe(0);
-    expect(window.analytics.identify.mock.calls.length).toBe(1);
-    expect(window.analytics.identify).toBeCalledWith(testTraits);
-  });
+      it('calls Segment track on success after identifyAnonymousUser', () => {
+        testSendTrackEventAfterIdentify(identifyAnonymousUser);
+      });
 
-  it('calls Segment identify on success - previous segment user', () => {
-    window.analytics.user = () => ({ id: () => 7 });
-    const testTraits = { anything: 'Yay!' };
-    identifyAnonymousUser(testTraits);
+      it('fails if track called with no identify', () => {
+        sendTrackEvent();
 
-    expect(window.analytics.reset.mock.calls.length).toBe(1);
-    expect(window.analytics.identify.mock.calls.length).toBe(1);
-    expect(window.analytics.identify).toBeCalledWith(testTraits);
-  });
-});
-
-function testSendPageAfterIdentify(identifyFunction) {
-  identifyFunction();
-
-  const testCategory = 'test-category';
-  const testName = 'test-name';
-  const testProperties = { anything: 'Yay!' };
-  sendPageEvent(testCategory, testName, testProperties);
-
-  expect(window.analytics.page.mock.calls.length).toBe(1);
-  expect(window.analytics.page).toBeCalledWith(testCategory, testName, testProperties);
-}
-
-describe('analytics send Page event', () => {
-  it('calls Segment page on success after identifyAuthenticatedUser', () => {
-    const userId = 1;
-    testSendPageAfterIdentify(() => identifyAuthenticatedUser(userId));
+        expect(mockLoggingService.logError.mock.calls.length).toBe(1);
+        expect(mockLoggingService.logError).toBeCalledWith('Identify must be called before other tracking events.');
+      });
+    });
   });
 
-  it('calls Segment page on success after identifyAnonymousUser', () => {
-    testSendPageAfterIdentify(identifyAnonymousUser);
-  });
+  describe('with invalid SEGMENT_KEY', () => {
+    beforeEach(() => {
+      service = configure(SegmentAnalyticsService, {
+        loggingService: mockLoggingService,
+        httpClient: mockAuthApiClient,
+        config: {
+          LMS_BASE_URL: 'https://example.com',
+          SEGMENT_KEY: '',
+        },
+      });
 
-  it('fails if page called with no identify', () => {
-    sendPageEvent();
+      afterConfigure();
+    });
 
-    expect(mockLoggingService.logError.mock.calls.length).toBe(1);
-    expect(mockLoggingService.logError).toBeCalledWith('Identify must be called before other tracking events.');
-  });
-});
+    it('should not initialize Segment analytics', () => {
+      expect(global.analytics.invoked).toBeFalsy();
+      expect(service.segmentInitialized).toBe(false);
+    });
 
-function testSendTrackEventAfterIdentify(identifyFunction) {
-  identifyFunction();
+    it('should not call segment track in sendTrackEvent', () => {
+      sendTrackEvent();
+      expect(window.analytics.track).not.toHaveBeenCalled();
+    });
 
-  const testName = 'test-name';
-  const testProperties = { anything: 'Yay!' };
-  sendTrackEvent(testName, testProperties);
+    it('should not call segment page in sendPageEvent', () => {
+      sendPageEvent();
+      expect(window.analytics.page).not.toHaveBeenCalled();
+    });
 
-  expect(window.analytics.track.mock.calls.length).toBe(1);
-  expect(window.analytics.track).toBeCalledWith(testName, testProperties);
-}
+    it('should not call ready, user, reset, or identify in identifyAnonymousUser', () => {
+      identifyAnonymousUser();
 
-describe('analytics send Track event', () => {
-  it('calls Segment track on success after identifyAuthenticatedUser', () => {
-    const userId = 1;
-    testSendTrackEventAfterIdentify(() => identifyAuthenticatedUser(userId));
-  });
+      expect(window.analytics.ready).not.toHaveBeenCalled();
+      expect(window.analytics.user).not.toHaveBeenCalled();
+      expect(userIdFunction).not.toHaveBeenCalled();
+      expect(window.analytics.reset).not.toHaveBeenCalled();
+      expect(window.analytics.identify).not.toHaveBeenCalled();
+      expect(service.hasIdentifyBeenCalled).toBe(false);
+    });
 
-  it('calls Segment track on success after identifyAnonymousUser', () => {
-    testSendTrackEventAfterIdentify(identifyAnonymousUser);
-  });
+    it('should not call identify in identifyAuthenticatedUser', () => {
+      identifyAuthenticatedUser(testUserId);
 
-  it('fails if track called with no identify', () => {
-    sendTrackEvent();
-
-    expect(mockLoggingService.logError.mock.calls.length).toBe(1);
-    expect(mockLoggingService.logError).toBeCalledWith('Identify must be called before other tracking events.');
+      expect(window.analytics.identify).not.toHaveBeenCalled();
+      expect(service.hasIdentifyBeenCalled).toBe(false);
+    });
   });
 });
