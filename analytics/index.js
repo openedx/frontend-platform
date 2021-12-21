bc8a9526ecdc9e3851caed3ece3a204c5e4b1596
davidjoy <davidjoy@users.noreply.github.com>
2021-12-21 19:24:56 +0000

    Deploying to gh-pages from @ openedx/frontend-platform@fca0049757f2af046eac05f5b147388f71a8665f 🚀

--- analytics/index.js
+++
@@ -1,4 +0,0 @@
-export { configure, identifyAnonymousUser, identifyAuthenticatedUser, sendPageEvent, sendTrackEvent, sendTrackingLogEvent, getAnalyticsService, resetAnalyticsService } from './interface';
-export { default as SegmentAnalyticsService } from './SegmentAnalyticsService';
-export { default as MockAnalyticsService } from './MockAnalyticsService';
-//# sourceMappingURL=index.js.map
