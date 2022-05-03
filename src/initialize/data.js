ec07460a074dca920bc1cf67cdbceda7b6302a25
Diana Catalina Olarte <diana.olarte@edunext.co>
2022-05-03 12:20:54 -0500

    feat: conect to eox-tenant api

--- src/initialize/data.js
+++ src/initialize/data.js
@@ -1,14 +1,15 @@
 import configureCache from "../auth/LocalForageCache";
+import { getConfig } from "../config";
 
-const capiConfig = {
+const apiConfig = {
     headers: { accept: 'application/json' }
 };
 
+
 export async function getHttpConfig(LMS_BASE_URL) {
     // url something like https://{LMS_BASE_URL}/api/v1/mfe-settings
-    // now use mock api
-    const apiService = await configureCache()
-    const url = `http://localhost:3000/config/${LMS_BASE_URL}`;
-    const { data } = await apiService.get(url, capiConfig);
+    const apiService = await configureCache();
+    const url = getConfig().ENVIRONMENT == "development" ? `http://${LMS_BASE_URL}:8000/eox-tenant/api/v1/mfe-api/` : `https://${LMS_BASE_URL}eox-tenant/api/v1/mfe-api/${LMS_BASE_URL}`;
+    const { data } = await apiService.get(url, apiConfig);
     return data;
 }
