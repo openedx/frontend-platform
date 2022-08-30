caa87e1e397c227ab761bacb5f6306e781a10883
Long Lin <lin.longsheng01@gmail.com>
2022-08-30 16:59:22 -0400

    feat: use axios cache interceptor for cache implementation

--- src/auth/LocalForageCache.js
+++ src/auth/LocalForageCache.js
@@ -1,7 +1,13 @@
 /* eslint-disable no-underscore-dangle */
 import localforage from 'localforage';
 import memoryDriver from 'localforage-memoryStorageDriver';
-import { setup } from 'axios-cache-adapter';
+import {
+  setupCache,
+  defaultKeyGenerator,
+  defaultHeaderInterpreter,
+  buildStorage,
+} from 'axios-cache-interceptor';
+import axios from 'axios';
 
 /**
  * Async function to configure localforage and setup the cache
@@ -24,17 +30,49 @@ export default async function configureCache() {
     name: 'edx-cache',
   });
 
-  // Set up the cache with a default maxAge of 5 minutes and using localforage as the storage source
-  return setup({
-    cache: {
-      maxAge: 5 * 60 * 1000,
-      store: forageStore,
-      exclude: { query: false },
-      invalidate: async (config, request) => {
-        if (request.clearCacheEntry) {
-          await config.store.removeItem(config.uuid);
-        }
-      },
+  const forageStoreAdapter = buildStorage({
+    async find(key) {
+      const result = await forageStore.getItem(`axios-cache:${key}`);
+      return JSON.parse(result);
+    },
+
+    async set(key, value) {
+      await forageStore.setItem(`axios-cache:${key}`, JSON.stringify(value));
+    },
+
+    async remove(key) {
+      await forageStore.removeItem(`axios-cache:${key}`);
     },
   });
+
+  // only GET methods are cached by default
+  return setupCache(
+    // axios instance
+    axios.create(),
+    {
+      ttl: 5 * 60 * 1000, // default maxAge of 5 minutes
+      // The storage to save the cache data. There are more available by default.
+      //
+      // https://axios-cache-interceptor.js.org/#/pages/storages
+      storage: forageStoreAdapter,
+
+      // The mechanism to generate a unique key for each request.
+      //
+      // https://axios-cache-interceptor.js.org/#/pages/request-id
+      generateKey: defaultKeyGenerator,
+
+      // The mechanism to interpret headers (when cache.interpretHeader is true).
+      //
+      // https://axios-cache-interceptor.js.org/#/pages/global-configuration?id=headerinterpreter
+      headerInterpreter: defaultHeaderInterpreter,
+
+      // The function that will receive debug information.
+      // NOTE: For this to work, you need to enable development mode.
+      //
+      // https://axios-cache-interceptor.js.org/#/pages/development-mode
+      // https://axios-cache-interceptor.js.org/#/pages/global-configuration?id=debug
+      // eslint-disable-next-line no-console
+      debug: console.log,
+    },
+  );
 }
