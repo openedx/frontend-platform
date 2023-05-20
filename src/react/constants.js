06eada12731a748496aaa300c3af3541e3ebc5cf
Adam Stankiewicz <agstanki@gmail.com>
2023-05-20 10:28:59 -0400

    feat: add ability to dynamically load theme overrides
    feat: adds support for loading external theme CSS for MFEs
    
    Introduces `useAppTheme` in `AppProvider` to load/inject `<link>` elements for the core theme CSS and any theme variant CSS into the HTML document. Exposes the app theme state and a way to mutate it to consumers via `AppContext`.
    
    fix: handle missing theme config
    
    fix: add env vars to env files
    
    fix: remove unused code
    
    chore: wip commit
    
    fix: prefer runtime config for paragon theme
    
    fix: rebase on master and resolve package conflicts
    
    fix: revert to original webpack.dev.config config
    
    chore: clean up unnecessary comment line
    
    fix: grammar
    
    Co-authored-by: Adolfo R. Brandes <arbrandes@arbrand.es>

---
+++ src/react/constants.js
@@ -0,0 +1,5 @@
+export const PARAGON_THEME_CORE = 'core';
+export const PARAGON_THEME_VARIANT_LIGHT = 'light';
+
+export const SET_THEME_VARIANT = 'SET_THEME_VARIANT';
+export const SET_IS_THEME_LOADED = 'SET_IS_THEME_LOADED';
