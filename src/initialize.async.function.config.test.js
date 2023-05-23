7cc71559fa90579fad92aebc61eb18fcc9e1e32a
David Joy <davjoy@gmail.com>
2023-05-23 15:50:59 -0400

    test: improving test coverage for js file config
    Turns out if you put each test in its own file, jest.mock can be used to change the implementation of env.config.js for each test file.  This lets us test all the clauses in jsFileConfig.  It also means we no longer need the actual env.config.js file, since we’re mocking it!

---
+++ src/initialize.async.function.config.test.js
@@ -0,0 +1,44 @@
+import PubSub from 'pubsub-js';
+import { initialize } from './initialize';
+
+import {
+  logError,
+} from './logging';
+import {
+  ensureAuthenticatedUser,
+  fetchAuthenticatedUser,
+  hydrateAuthenticatedUser,
+} from './auth';
+import { getConfig } from './config';
+
+jest.mock('./logging');
+jest.mock('./auth');
+jest.mock('./analytics');
+jest.mock('./i18n');
+jest.mock('./auth/LocalForageCache');
+jest.mock('env.config.js', () => async () => new Promise((resolve) => {
+  resolve({
+    JS_FILE_VAR: 'JS_FILE_VAR_VALUE_ASYNC_FUNCTION',
+  });
+}));
+
+let config = null;
+
+describe('initialize with async function js file config', () => {
+  beforeEach(() => {
+    jest.resetModules();
+    config = getConfig();
+    fetchAuthenticatedUser.mockReset();
+    ensureAuthenticatedUser.mockReset();
+    hydrateAuthenticatedUser.mockReset();
+    logError.mockReset();
+    PubSub.clearAllSubscriptions();
+  });
+
+  it('should initialize the app with async function javascript file configuration', async () => {
+    const messages = { i_am: 'a message' };
+    await initialize({ messages });
+
+    expect(config.JS_FILE_VAR).toEqual('JS_FILE_VAR_VALUE_ASYNC_FUNCTION');
+  });
+});
