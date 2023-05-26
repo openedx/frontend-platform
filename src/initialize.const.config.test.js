b18d520ef61e35941320611a533ac134a8c16c3e
David Joy <davidjoy@users.noreply.github.com>
2023-05-26 10:33:48 -0400

    feat: JavaScript file configuration of apps (#474)
    * feat: JavaScript file configuration of apps
    
    This adds the ability to configure an application via an env.config.js file rather than environment variables or .env files.  This mechanism is much more flexible and powerful than environment variables as described in the associated ADR.
    
    Note that this is not in _use_ by any MFEs as of this PR being merged - this merely adds the capability.  Guidance will be forthcoming on how to use this mechanism, along with a DEPR issue for the deprecation of environment variable configuration later this year.  
    
    It also improves documentation around how to configure applications, providing more detail on the various methods.
    
    Finally, it allows the logging, analytics, and auth services to be configured via the configuration document now that we can supply an alternate implementation in an env.config.js file.  This allows configuration of these services without forking the MFE.
    
    We have agreed we’d like to DEPR environment variable config, so I’m going to merge the ADR as “Accepted”

---
+++ src/initialize.const.config.test.js
@@ -0,0 +1,42 @@
+import PubSub from 'pubsub-js';
+import { initialize } from './initialize';
+
+import {
+  logError,
+} from './logging';
+import {
+  ensureAuthenticatedUser,
+  fetchAuthenticatedUser,
+  hydrateAuthenticatedUser,
+} from './auth';
+import { getConfig } from './config';
+
+jest.mock('./logging');
+jest.mock('./auth');
+jest.mock('./analytics');
+jest.mock('./i18n');
+jest.mock('./auth/LocalForageCache');
+jest.mock('env.config.js', () => ({
+  JS_FILE_VAR: 'JS_FILE_VAR_VALUE_CONSTANT',
+}));
+
+let config = null;
+
+describe('initialize with constant js file config', () => {
+  beforeEach(() => {
+    jest.resetModules();
+    config = getConfig();
+    fetchAuthenticatedUser.mockReset();
+    ensureAuthenticatedUser.mockReset();
+    hydrateAuthenticatedUser.mockReset();
+    logError.mockReset();
+    PubSub.clearAllSubscriptions();
+  });
+
+  it('should initialize the app with javascript file configuration', async () => {
+    const messages = { i_am: 'a message' };
+    await initialize({ messages });
+
+    expect(config.JS_FILE_VAR).toEqual('JS_FILE_VAR_VALUE_CONSTANT');
+  });
+});
