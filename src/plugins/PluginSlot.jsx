766a24c81728172f3f3aa696a767f0ef27080eb1
David Joy <davjoy@gmail.com>
2023-06-07 14:46:18 -0400

    feat: adding a default fallback component to plugins
    It defaults to a centered Spinner component from Paragon.  Note that there’s a TODO here around internationalizing the “Loading” screen reader text for the spinner.  This will have to wait for us to add i18n messages to frontend-platform.

--- src/plugins/PluginSlot.jsx
+++ src/plugins/PluginSlot.jsx
@@ -1,13 +1,29 @@
 import React, { forwardRef } from 'react';
+
+import classNames from 'classnames';
+import { Spinner } from '@edx/paragon';
 import PropTypes from 'prop-types';
+import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 
 import { usePluginSlot } from './data/hooks';
 import PluginContainer from './PluginContainer';
 
 const PluginSlot = forwardRef(({
-  as, id, pluginProps, children, ...props
+  as, id, intl, pluginProps, children, ...props
 }, ref) => {
   const { plugins, keepDefault } = usePluginSlot(id);
+
+  const { fallback } = pluginProps;
+
+  // TODO: Add internationalization to the "Loading" text on the spinner.
+  let finalFallback = (
+    <div className={classNames(pluginProps.className, 'd-flex justify-content-center align-items-center')}>
+      <Spinner animation="border" screenReaderText="Loading" />
+    </div>
+  );
+  if (fallback !== undefined) {
+    finalFallback = fallback;
+  }
 
   let finalChildren = [];
   if (plugins.length > 0) {
@@ -15,7 +31,14 @@ const PluginSlot = forwardRef(({
       finalChildren.push(children);
     }
     plugins.forEach((pluginConfig) => {
-      finalChildren.push(<PluginContainer key={pluginConfig.url} config={pluginConfig} {...pluginProps} />);
+      finalChildren.push(
+        <PluginContainer
+          key={pluginConfig.url}
+          config={pluginConfig}
+          fallback={finalFallback}
+          {...pluginProps}
+        />,
+      );
     });
   } else {
     finalChildren = children;
@@ -31,16 +54,18 @@ const PluginSlot = forwardRef(({
   );
 });
 
-export default PluginSlot;
+export default injectIntl(PluginSlot);
 
 PluginSlot.propTypes = {
-  id: PropTypes.string.isRequired,
   as: PropTypes.elementType,
   children: PropTypes.node,
+  id: PropTypes.string.isRequired,
+  intl: intlShape.isRequired,
   pluginProps: PropTypes.object, // eslint-disable-line
 };
 
 PluginSlot.defaultProps = {
   as: 'div',
   children: null,
+  pluginProps: {},
 };
