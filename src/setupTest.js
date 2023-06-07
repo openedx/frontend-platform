9d4817ce054d36bdd7b64463618fcbf035d3d83a
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2023-06-07 17:59:51 +0500

    feat: upgraded react & react-dom to v17, added enzyme-adapter-react-17

--- src/setupTest.js
+++ src/setupTest.js
@@ -3,7 +3,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
 
