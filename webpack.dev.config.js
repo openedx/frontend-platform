20deb19741520060030149b0a841b85575919dcc
Adam Stankiewicz <agstanki@gmail.com>
2024-08-30 16:27:03 -0400

    fix: define app entry point for webpack.dev.config.js

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 const { createConfig } = require('@openedx/frontend-build');
 
 module.exports = createConfig('webpack-dev', {
-  entry: path.resolve(__dirname, 'example'),
+  entry: {
+    app: path.resolve(__dirname, 'example'),
+  },
   output: {
     path: path.resolve(__dirname, 'example/dist'),
     publicPath: '/',
