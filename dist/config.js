57c209f85c371c4f8696393a6b3a74760f35cca0
Diana Olarte <diana.olarte@edunext.co>
2024-12-03 08:34:12 +1100

    test: regenerate dist folder for testing

--- dist/config.js
+++ dist/config.js
@@ -167,7 +167,8 @@ var config = {
   FAVICON_URL: process.env.FAVICON_URL,
   MFE_CONFIG_API_URL: process.env.MFE_CONFIG_API_URL,
   APP_ID: process.env.APP_ID,
-  SUPPORT_URL: process.env.SUPPORT_URL
+  SUPPORT_URL: process.env.SUPPORT_URL,
+  PARAGON_THEME_URLS: JSON.parse(process.env.PARAGON_THEME_URLS) || {}
 };
 
 /**
