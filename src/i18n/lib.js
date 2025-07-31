ae078a9603c70fda01771b8432e8fe26711a465d
Vu The Huy <vuthehuy.t61@gmail.com>
2025-07-31 21:50:42 +0700

    feat: add Vietnamese language support

--- src/i18n/lib.js
+++ src/i18n/lib.js
@@ -17,6 +17,7 @@ import '@formatjs/intl-pluralrules/locale-data/pt';
 import '@formatjs/intl-pluralrules/locale-data/ru';
 import '@formatjs/intl-pluralrules/locale-data/th';
 import '@formatjs/intl-pluralrules/locale-data/uk';
+import '@formatjs/intl-pluralrules/locale-data/vi';
 
 import '@formatjs/intl-relativetimeformat/polyfill';
 import '@formatjs/intl-relativetimeformat/locale-data/ar';
@@ -33,6 +34,7 @@ import '@formatjs/intl-relativetimeformat/locale-data/pt';
 import '@formatjs/intl-relativetimeformat/locale-data/ru';
 import '@formatjs/intl-relativetimeformat/locale-data/th';
 import '@formatjs/intl-relativetimeformat/locale-data/uk';
+import '@formatjs/intl-relativetimeformat/locale-data/vi';
 
 const cookies = new Cookies();
 const supportedLocales = [
@@ -52,6 +54,7 @@ const supportedLocales = [
   'ru', // Russian
   'th', // Thai
   'uk', // Ukrainian
+  'vi', // Vietnamese
 ];
 const rtlLocales = [
   'ar', // Arabic
@@ -220,6 +223,7 @@ const messagesShape = {
   ru: PropTypes.objectOf(PropTypes.string), // Russian
   th: PropTypes.objectOf(PropTypes.string), // Thai
   uk: PropTypes.objectOf(PropTypes.string), // Ukrainian
+  vi: PropTypes.objectOf(PropTypes.string), // Vietnamese
 };
 
 const optionsShape = {
